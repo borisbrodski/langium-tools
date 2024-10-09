cee9a62c84b321719874bec67efbb0d4d789a645
Boris Brodski <brodsky_boris@yahoo.com>
2024-10-09 15:31:01 +0200

    Typedocs for lang/java

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * as base from './base/index.js';
 export * as generator from './generator/index.js';
 export * as testing from './testing/index.js';
+export * as lang from './lang/index.js';
 
